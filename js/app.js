39f63308f444cca0007f2fc7e2936b8a548f403a
damgau <damien.gauthier3003@gmail.com>
2016-03-29 12:28:20 +0200

    fin du cours

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
+
     $('#fullpage').fullpage({
         anchors:['home', 'about', 'work','contact']
+    });
+
+    $('.slider').slick({ 
+    	dots: true,
+    	arrows : false,
+    	 });
+    $(".btn-toggle-menu").click(function() {
+    	$(".main_menu").toggleClass("hide-menu");
     });
 });
